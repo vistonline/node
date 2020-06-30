629aec0108078e6c14b910ed87087a959bf401cf
Jorge Goulart <jorgegru@gmail.com>
2020-06-30 13:12:25 -0300

    Corrigindo erro

--- config/server.js
+++ config/server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const cors = require('cors');
-const multer = require('multer');
+// const multer = require('multer');
 
 const consign = require('consign');
 const bodyParser = require('body-parser');
-var upload = multer();
+// var upload = multer();
 
-app.set('view engine', 'ejs');
-app.use(express.static('./src/public'));
 app.use(express.json());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(upload.array());
+// app.use(upload.array());
 app.use(cors({
     origin: '*'
 }))
+// app.set('view engine', 'ejs');
+// app.use(express.static('./src/public'));
 
 consign()
     .include('./src/controllers')
--- src/controllers/ws.js
+++ src/controllers/ws.js
@@ -15,7 +15,6 @@ module.exports =  app => {
 
         var args = req.body;
         console.log(args)
-        return res.send({args});
         await soap.createClient(url, async (err, client) => {
             try {
                 if (client[action]) {
